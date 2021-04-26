ef07ea93749247675a7831505d072f9c1f5900ee
DoellBarr <shohih242@gmail.com>
2021-04-26 10:46:53 +0700

    Update Callsmusic

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from pytgcalls import PyTgCalls
 
 from config import SESSION_NAME, API_ID, API_HASH
-from queues import get, task_done, is_empty
+from .queues import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
@@ -10,14 +10,14 @@ pytgcalls = PyTgCalls(client)
 
 @pytgcalls.on_stream_end()
 def on_stream_end(chat_id: int) -> None:
-    task_done(chat_id)
+    queues.task_done(chat_id)
 
-    if is_empty(chat_id):
+    if queues.is_empty(chat_id):
         pytgcalls.leave_group_call(chat_id)
         client.leave_chat(chat_id)
     else:
         pytgcalls.change_stream(
-            chat_id, get(chat_id)["file"]
+            chat_id, queues.get(chat_id)["file"]
         )
 
 
