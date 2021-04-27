b65c29cdae17649acc742210b2518a5f2b767cbc
DoellBarr <shohih242@gmail.com>
2021-04-27 09:47:43 +0700

    Bismillah

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -11,10 +11,8 @@ pytgcalls = PyTgCalls(client)
 @pytgcalls.on_stream_end()
 def on_stream_end(chat_id: int) -> None:
     queues.task_done(chat_id)
-
     if queues.is_empty(chat_id):
         pytgcalls.leave_group_call(chat_id)
-        await client.leave_chat(chat_id)
     else:
         pytgcalls.change_stream(
             chat_id, queues.get(chat_id)["file"]
--- handlers/chat_member_updated.py
+++ handlers/chat_member_updated.py
@@ -1,29 +1,17 @@
 from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated
 
-from cache.admins import admins
+from cache import admins
 
 
 @Client.on_chat_member_updated()
 async def auto_admin_updater(_, chat_member_updated: ChatMemberUpdated):
-    (
-        admins[chat_member_updated.chat.id].append(
-            chat_member_updated.new_chat_member.user.id
-        )
-    ) if (
-            (
-                chat_member_updated.new_chat_member.can_manage_voice_chats
-            ) and (
-                    (
-                        chat_member_updated.new_chat_member.user.id
-                    ) not in admins[chat_member_updated.chat.id]
-            )
-    ) else (
-        admins[chat_member_updated.chat.id].remove(
-            chat_member_updated.new_chat_member.user.id
-        )
-    ) if (
-            (
-                chat_member_updated.new_chat_member.user.id
-            ) in admins[chat_member_updated.chat.id]
-    ) else None
+    chat, new = chat_member_updated.chat, chat_member_updated.new_chat_member
+    if new:
+        if chat.id in admins:
+            if new.can_manage_voice_chats:
+                if new.user.id not in admins.admins[chat.id]:
+                    admins.admins[chat.id].append(new.user.id)
+            else:
+                if new.user.id in admins.admins[chat.id]:
+                    admins.admins[chat.id].remove(new.user.id)
