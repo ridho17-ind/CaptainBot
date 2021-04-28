c05b8b6b9214b70236b8cb8202f22e4d1d5ce353
DoellBarr <shohih242@gmail.com>
2021-04-28 13:49:56 +0700

    hayu

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -28,14 +28,14 @@ async def ytsearch(_, message: Message):
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("Mencari....")
         results = YoutubeSearch(query, max_results=4).to_dict()
-        i = 1
+        i = 0
         text = ""
-        while i <= 4:
-            text += f"{i}.  Judul - {results[i]['title']}\n"
-            text += f"      Durasi - {results[i]['duration']}\n"
-            text += f"      Penonton - {results[i]['views']}\n"
-            text += f"      Channel - {results[i]['channel']}\n"
-            text += f"      https://youtube.com{results[i]['url_suffix']}\n\n"
+        while i < 4:
+            text += f"Judul - {results[i]['title']}\n"
+            text += f"Durasi - {results[i]['duration']}\n"
+            text += f"Penonton - {results[i]['views']}\n"
+            text += f"Channel - {results[i]['channel']}\n"
+            text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(
             [
