299e65738a73f5db9338278d361f57c29461535f
DoellBarr <shohih242@gmail.com>
2021-04-28 13:35:34 +0700

    hayu

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -29,7 +29,6 @@ async def ytsearch(_, message: Message):
         m = await message.reply_text("Mencari....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
-        mar = InlineKeyboardButton(f"{i}", url=f"https://youtube.com{results[i]['url_suffix']}"),
         text = ""
         while i < 4:
             text += f"{i}.  Judul - {results[i]['title']}\n"
@@ -37,9 +36,16 @@ async def ytsearch(_, message: Message):
             text += f"      Penonton - {results[i]['views']}\n"
             text += f"      Channel - {results[i]['channel']}\n"
             text += f"      https://youtube.com{results[i]['url_suffix']}\n\n"
-            moa = mar
-            print(moa)
             i += 1
-        await m.edit(text, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[moa]]))
+        await m.edit(text, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"1", url=f"https://youtube.com{results[0]['url_suffix']}"),
+                    InlineKeyboardButton(f"2", url=f"https://youtube.com{results[1]['url_suffix']}"),
+                    InlineKeyboardButton(f"3", url=f"https://youtube.com{results[2]['url_suffix']}"),
+                    InlineKeyboardButton(f"4", url=f"https://youtube.com{results[3]['url_suffix']}"),
+                ]
+            ]
+        ))
     except Exception as e:
         await message.reply_text(str(e))
